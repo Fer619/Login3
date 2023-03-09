b7b26c7d8e9576f0cd8aa92e859607ffabefb694
Ramon Alejandro Aguirre Romero <73568821+LexAguirre@users.noreply.github.com>
2023-03-09 00:23:58 -0600

    Updated key

--- src/keys.js
+++ src/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongodb: {
-        URI: 'mongodb+srv://FBS:<12345>@login0.lg2ceeo.mongodb.net/test'
-    }
-}
+  mongodb: {
+    URI: "mongodb+srv://FBS:12345@login0.lg2ceeo.mongodb.net/test",
+  },
+};
